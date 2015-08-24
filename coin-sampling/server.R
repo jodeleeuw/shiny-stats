9c8f999c40ae39ff6f9c1d6814b3555199f2f30f
Josh de Leeuw <josh.deleeuw@gmail.com>
2015-08-24 15:16:11 -0400

    update coin sampling app

--- coin-sampling/server.R
+++ coin-sampling/server.R
@@ -6,7 +6,7 @@
 #
 
 library(shiny)
-library(shinyjs)
+#library(shinyjs)
 library(ggplot2)
 
 shinyServer(function(input, output, session) {
@@ -54,12 +54,60 @@ shinyServer(function(input, output, session) {
     data <- data.frame(table(rv$outcomes))
     colnames(data) <- c("val","freq")
     
-    p <- ggplot(data, aes(x=val,y=freq)) +
+    b <- 0:input$numCoins
+    v <- sapply(b, function(b){
+      if(input$rangeType == 'inside'){
+        if(b >= input$range[1] & b <= input$range[2]){
+          return("red")
+        } else {
+          return("black")
+        }
+      } else {
+        if(b >= input$range[1] & b <= input$range[2]){
+          return("black")
+        } else {
+          return("red")
+        }
+      }
+    })
+    
+    if(input$numCoins >= 10000){
+      byval <- 25
+    } else if(input$numCoins >= 1000){
+      byval <- 10
+    } else if(input$numCoins >= 500){
+      byval <- 5
+    } else if(input$numCoins >= 100){
+      byval <- 2
+    } else {
+      byval <- 1
+    }
+    
+    lab_breaks <- seq(from=0, to=input$numCoins, by=byval)
+    
+    
+    p <- ggplot(data, aes(x=val,y=freq, fill=val)) +
       geom_bar(stat="identity")+
       labs(y="# of trials\n",x="\n# of heads in trial")+
-      theme_bw()
+      scale_fill_manual(guide=F, limits=b,values=v, drop=F)+
+      scale_x_discrete(breaks=lab_breaks)+
+      theme_minimal(base_size=18)
     return(p)
 
+  })
+  
+  output$rangeInfo <- renderText({
+    if(input$rangeType == 'inside'){
+      v <- sum(rv$outcomes >= input$range[1] & rv$outcomes <= input$range[2])
+    } else {
+      v <- sum(rv$outcomes < input$range[1] | rv$outcomes > input$range[2])
+    }
+    p <- v / length(rv$outcomes)*100
+    if(is.nan(p)){
+      return("Flip some coins to see the result!")
+    } else {
+      return(paste0("There have been ",length(rv$outcomes)," runs of the simulation. ",round(p,digits=2),"% of the outcomes meet the selection criteria."))
+    }
   })
   
   observe({
@@ -71,6 +119,12 @@ shinyServer(function(input, output, session) {
       enable("numCoins")
       enable("probHeads")
     }
+  })
+  
+  output$evaluationPanel <- renderUI({
+    maxV <- input$numCoins
+    qV <- round(maxV / 4)
+    sliderInput("range",label="of the following range", min=0,max=input$numCoins,step=1,value=c(qV,input$numCoins-qV))
   })
 
 })
--- coin-sampling/ui.R
+++ coin-sampling/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyjs)
 library(shinythemes)
 
 ## helper function for styled buttons
@@ -17,25 +18,34 @@ shinyUI(fluidPage(theme=shinytheme("journal"),
   titlePanel("Coin Flip Simulation"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      numericInput("numCoins", "How many coins to flip per trial?",1,min=1,step=1),
-      sliderInput("probHeads", "Probability of heads?",min=0,max=1,value=0.5),
-      fluidRow(
-        column(12,
-        actionButton("flip1", "Run 1", css.class="btn-sm"),
-        actionButton("flip10", "Run 10", css.class="btn-sm"),
-        actionButton("flip100", "Run 100", css.class="btn-sm"),
-        actionButton("flip1000", "Run 1000", css.class="btn-sm"),
-        class="form-group")
+  fluidRow(
+    column(4,
+      wellPanel(
+        numericInput("numCoins", "How many coins to flip per trial?",10,min=1,step=1),
+        numericInput("probHeads", "Probability of heads?",min=0,max=1,value=0.5, step=0.001),
+        fluidRow(
+          column(12,
+          actionButton("flip1", "Run 1", css.class="btn-sm"),
+          actionButton("flip10", "Run 10", css.class="btn-sm"),
+          actionButton("flip100", "Run 100", css.class="btn-sm"),
+          actionButton("flip1000", "Run 1000", css.class="btn-sm"),
+          class="form-group")
+        ),
+        fluidRow(column(12,style="text-align:center",class="form-group", 
+                        actionButton("reset","Reset Simulation", css.class="btn-sm")))
       ),
-      fluidRow(column(12,style="text-align:center",class="form-group", 
-                      actionButton("reset","Reset Simulation", css.class="btn-sm")))
+      wellPanel(
+        selectInput("rangeType", "Show the % of outcomes that are", c("inside", "outside"), selected="inside"),
+        uiOutput('evaluationPanel')
+      )
     ),
 
     # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+    column(8,
+      plotOutput("distPlot"),
+      wellPanel(
+        textOutput('rangeInfo')
+      )
     )
   )
 ))
