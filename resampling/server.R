dc2e41f17ae47b1f984e13183deab87f99bde737
rmbest <rmbestcogs@gmail.com>
2015-10-02 11:48:52 -0400

    Added resampling app

---
+++ resampling/server.R
@@ -0,0 +1,303 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(ggplot2)
+
+# Function for getting the means when resampling
+getMean <- function(val, order){
+  getDf <- data.frame(val, order)
+  m <- mean(getDf[getDf[,"order"]=="A","val"]) - mean(getDf[getDf[,"order"]=="B","val"])
+  return(m)
+}
+
+shinyServer(function(input, output) {
+  
+  rv <- reactiveValues(
+    outcomes = numeric(),
+    swapped = numeric(),
+    started = F,
+    newOrder = character()
+    # swapH = numeric() 
+  )
+  
+  # hotable for group A
+  output$tblA <- renderHotable({
+    timesA <- as.numeric(input$obsA)
+    if(is.na(timesA) | timesA <1){
+      timesA <- 1
+    }
+    datA <- data.frame(Obs = c(1:timesA), A = as.numeric(c(rep(NA, timesA))))
+    return(datA)
+  }, readOnly = c(TRUE, FALSE))
+  # hotable for group B
+  output$tblB <- renderHotable({
+    timesB <- as.numeric(input$obsB)
+    if(is.na(timesB) | timesB <1){
+      timesB <- 1
+    }
+    datA <- data.frame(Obs = c(1:timesB), B = as.numeric(c(rep(NA, timesB))))
+    return(datA)
+  }, readOnly = FALSE)
+  
+  # table to show the means and mean difference of the groups
+  tab <- reactive({
+    dataA <- hot.to.df(input$tblA)
+    dataB <- hot.to.df(input$tblB)
+    dataA$A <- as.numeric(dataA$A)
+    dataB$B <- as.numeric(dataB$B)
+    GroupA_mean <- mean(dataA$A)
+    GroupB_mean <- mean(dataB$B)
+    Difference <- GroupA_mean - GroupB_mean
+    tabl <- data.frame(GroupA_mean, GroupB_mean, Difference)
+    return(tabl)
+  })  
+  output$table <- renderTable({
+    tab()
+  })
+  
+  # reset things if they enter new data  
+  observeEvent(input$tblA, {
+    rv$outcomes = c()
+    rv$started = F
+    rv$newOrder = c()
+    # rv$swapH = c()
+  })
+  
+  observeEvent(input$tblB, {
+    rv$outcomes = c()
+    rv$started = F
+    rv$newOrder = c()
+    # rv$swapH = c()
+  })
+  
+  observeEvent(input$obsA, {
+    rv$outcomes = c()
+    rv$started = F
+    rv$newOrder = c()
+    # rv$swapH = c()
+  })
+  
+  observeEvent(input$obsB, {
+    rv$outcomes = c()
+    rv$started = F
+    rv$newOrder = c()
+    # rv$swapH = c()
+  })
+  
+  # run the resampler
+  observeEvent(input$flip1, {
+    resample(1)
+  })
+  
+  observeEvent(input$flip10, {
+    resample(10)
+  })
+  
+  observeEvent(input$flip100, {
+    resample(100)
+  })
+  
+  observeEvent(input$flip1000, {
+    resample(1000)
+  })
+  
+  observeEvent(input$flip10000, {
+    resample(10000)
+  })
+  
+  
+  resample <- function(n){
+    #for each resampling it shuffles data and gets the mean difference
+    rv$started <- T
+    dataA <- hot.to.df(input$tblA)
+    dataA$A <- as.numeric(dataA$A)
+    dataB <- hot.to.df(input$tblB)
+    dataB$B <- as.numeric(dataB$B)
+    val <- c(as.numeric(dataA[!is.na(dataA[,"A"]),"A"]), as.numeric(dataB[!is.na(dataB[,"B"]),"B"]))
+    group <- c(rep("A", length(dataA[!is.na(dataA[,"A"]),"A"])), rep("B", length(dataB[!is.na(dataB[,"B"]),"B"])) )
+    orders <- replicate(n, sample(group))
+    rv$newOrder <- orders[,n]
+    m <- apply(orders, MARGIN = 2, FUN = getMean, val = val )
+    rv$outcomes <- c(rv$outcomes, m)
+  }
+  
+  # Plot the resampled data points
+  output$groupsPlot <- renderPlot({
+    As <- hot.to.df(input$tblA)
+    Bs <- hot.to.df(input$tblB)
+    Values <- c(As[!is.na(As[,"A"]),"A"], Bs[!is.na(Bs[,"B"]),"B"])
+    
+    # if there's no data, the plot tells them something's funny
+    if(length(Values)<2){
+      Values <- c(3,3)
+    }
+    OriginalGroup <- c(rep("A", length(As[!is.na(As[,"A"]),"A"])), rep("B", length(Bs[!is.na(Bs[,"B"]),"B"])))
+    if(length(OriginalGroup)<2){
+      OriginalGroup <- c("A", "B")
+    }
+    NewGroup <- rv$newOrder
+    if(!rv$started){
+      NewGroup <- OriginalGroup
+    }
+    
+    # data frame for the plot
+    def <- data.frame(Values, OriginalGroup, NewGroup)
+    
+    # find the size of the biggest bin (if binsize = 1)
+    limtab <- as.data.frame(table(Values))
+    yMax <- max(limtab[,"Freq"])
+    limtab$Values <- as.numeric(limtab$Values)
+    yRange <- max(max(Values) - min(Values), 1)
+    yLength <- length(Values)
+    
+    # scale the binsize
+    wid <- (yRange)/(1.5*log(yRange^1.5+1)+(yLength^.5))
+    
+    # find the size of the biggest bin (when binsize  = wid)
+    yVal <- limtab[limtab[,"Freq"]==yMax,"Values"]
+    yMax2 <- sum(limtab[abs(limtab[,"Values"]-yVal<=wid), "Freq"])
+    
+    # scale the (relative) size of the dots
+    maxRat <- .4/log(yMax2^.05+1)+.6*(1-(1000/((max(Values, 1))+1000)))
+    
+    # scale the x axis (otherwise it gets too small when range = 1)
+    limMin <- min(def$Values)-wid*(yLength)*.4/(log(yRange^.8+1))
+    limMax <- max(def$Values)+wid*(yLength)*.4/(log(yRange^.8+1))
+    
+    mu <- data.frame(grp.mean = c(mean(def[def[,"NewGroup"]=="A","Values"]), mean(def[def[,"NewGroup"]=="B","Values"])),
+                     NewGroup = c("A", "B"))
+    p <- ggplot(def, aes(x = Values, fill = OriginalGroup)) + 
+      geom_dotplot(binwidth=wid, stackgroups = TRUE, dotsize = maxRat, binpositions = "all") + 
+      facet_grid( NewGroup ~ .) + 
+      scale_y_continuous(name = "", breaks = NULL) + 
+      xlim(limMin, limMax) + 
+      theme(legend.position = "bottom") +
+      geom_vline(aes(xintercept=grp.mean), mu,
+                 linetype="dashed", color="black")
+    print(p)
+  })
+  
+  # plot the histogram of means
+  output$distPlot <- renderPlot({
+    if(length(rv$outcomes)==0){ return(NULL) }
+    dataA <- hot.to.df(input$tblA)
+    dataB <- hot.to.df(input$tblB)
+    values <- c(as.numeric(dataA[!is.na(dataA[,"A"]),"A"]), as.numeric(dataB[!is.na(dataB[,"B"]),"B"]))
+    data <- data.frame(table(rv$outcomes))
+    colnames(data) <- c("val","freq")
+    data$val <- as.numeric(as.character(data$val))
+    data$freq <- as.numeric(as.character(data$freq))
+    
+    if(anyNA(data)){
+      data <- data.frame(val = c(0), freq = c(0))
+    }
+    
+    if(input$displayType == 'number'){
+      rng <- input$range
+    } else if(input$displayType == 'percentile'){
+      rng <- quantile(rv$outcomes, probs = input$percentile/100, type =1)
+    }
+    
+    data$inrange <- sapply(data$val, function(b){
+      if(input$rangeType == 'inside'){
+        if(b >= rng[1] & b <= rng[2]){
+          return("red")
+        } else {
+          return("black")
+        }
+      } else {
+        if(b >= rng[1] & b <= rng[2]){
+          return("black")
+        } else {
+          return("red")
+        }
+      }
+    })
+    
+    data$inrange <- as.factor(data$inrange)
+    
+    fillv <- levels(data$inrange)
+    
+    if(is.null(values)){
+      values <- c(-1, 1)
+    }
+    lim <- max(sd(values), max(abs(rv$outcomes)))+1
+    if(length(rv$outcomes)>10){
+      lim <- max(abs(rv$outcomes))+1
+    }
+    
+    p <- ggplot(data, aes(x=val,y=freq, fill = inrange)) +
+      geom_bar(stat="identity")+
+      labs(y="# of resamples\n",x="\n mean of resample")+
+      scale_fill_manual(guide=F, values=fillv)+
+      #scale_x_discrete(breaks=lab_breaks)+
+      theme_minimal(base_size=18) + 
+      xlim(-lim, lim)
+    return(p)
+    
+  })
+  
+  output$evaluationPanel <- renderUI({
+    maxV <- ceiling(max(rv$outcomes))
+    if(is.infinite(maxV)){
+      maxV <- 0
+    }
+    minV <-floor(min(rv$outcomes))
+    if(is.infinite(minV)){
+      minV <- 0
+    }
+    if(length(rv$outcomes)<10){
+      if(abs(minV)>=abs(maxV)){
+        maxV <- -minV
+      } else {
+        minV <- -maxV
+      }
+    }
+    qV <- round(maxV / 2)
+    if(is.nan(qV)){
+      qV <- 0
+    }
+    sliderInput("range",label="the range", min=minV,max=maxV,step=0.01,value=c(minV+qV,maxV-qV))
+  })
+  
+  output$rangeInfo <- renderText({
+    if( length(rv$outcomes) == 0 ) { return("Run the simulation to see the result!") }
+    
+    if( input$displayType == 'number' ){
+      if(input$rangeType == 'inside'){
+        v <- sum(rv$outcomes >= input$range[1] & rv$outcomes <= input$range[2])
+      } else {
+        v <- sum(rv$outcomes < input$range[1] | rv$outcomes > input$range[2])
+      }
+      p <- v / length(rv$outcomes)*100
+      
+      return(paste0("There have been ",length(rv$outcomes)," runs of the simulation. ",round(p,digits=2),"% of the outcomes meet the selection criteria."))
+      
+    } else if( input$displayType == 'percentile'){
+      q <- quantile(rv$outcomes, probs = input$percentile/100, type =1)
+      
+      return(paste0("There have been ",length(rv$outcomes)," runs of the simulation.",
+                    "The ",input$percentile[1]," percentile is ",round(q[[1]], 2)," and the ",input$percentile[2]," percentile is ", round(q[[2]], 2),"."))
+    }
+    
+    
+    return(paste0("There have been ",length(rv$outcomes)," runs of the simulation. ",round(p,digits=2),"% of the outcomes meet the selection criteria. ",
+                  "The ",input$percentile," percentile is ",q,"."))
+    
+  })
+  
+  
+})
+
+
+# getMedian
+# possibly put the dotplot in the same part as the histogram, make it only show up if you run one 
+# maybe make a dotplot for the original data that just hangs out
+
+
+
---
+++ resampling/ui.R
@@ -0,0 +1,66 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(ggplot2)
+library(shinyjs)
+library(shinythemes)
+require(shinysky)
+library(shinyTable)
+
+## helper function for styled buttons
+actionButton <- function(inputId, label, btn.style = "" , css.class = "") {
+  if ( btn.style %in% c("primary","info","success","warning","danger","inverse","link")) {
+    btn.css.class <- paste("btn",btn.style,sep="-")
+  } else btn.css.class = ""
+  
+  tags$button(id=inputId, type="button", class=paste("btn action-button",btn.css.class,css.class,collapse=" "), label)
+}
+
+shinyUI(fluidPage(
+  titlePanel('Resampling'),
+  
+  sidebarPanel(
+    fluidRow(
+      column(5, numericInput("obsA", "Observations in Group A", 1, min = 1),
+             hotable("tblA")),
+      column(5, numericInput("obsB", "Observations in Group B", 1, min = 1), 
+             hotable("tblB") )),
+    br(),
+    column(12, plotOutput("groupsPlot") )
+    ,
+    tableOutput('table'),
+    fluidRow(
+      column(12,
+             actionButton("flip1", "Run 1", css.class="btn-sm"),
+             actionButton("flip10", "Run 10", css.class="btn-sm"),
+             actionButton("flip100", "Run 100", css.class="btn-sm"),
+             actionButton("flip1000", "Run 1000", css.class="btn-sm"),
+             actionButton("flip10000", "Run 10000", css.class="btn-sm"),
+             class="form-group")
+    )
+  ),
+  
+  mainPanel(
+    plotOutput("distPlot"),
+    wellPanel(
+      textOutput('rangeInfo'),
+      radioButtons('displayType', "Select range based on:",
+                   c("Difference of means" = "number",
+                     "Percentiles" = "percentile")),
+      selectInput("rangeType", "Select the outcomes that are", c("inside", "outside"), selected="inside"),
+      conditionalPanel('input.displayType == "number"',
+                       uiOutput('evaluationPanel')
+      ),
+      conditionalPanel('input.displayType == "percentile"',
+                       sliderInput("percentile", label="the percentile range", min=0,max=100,step=0.5, val = c(25,75))
+      )
+    )
+  )
+  
+  
+))
